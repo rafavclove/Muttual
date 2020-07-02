15839fdcb5a9b8bda9508e9ed08771bba586c983
Rafael Valencia Castellanos <raf.valenc@gmail.com>
2020-07-01 23:59:11 -0500

    Se vincula la vista al inicio

--- Muttual/Muttual/App.xaml.cs
+++ Muttual/Muttual/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Muttual.View;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace Muttual
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new PersonPage());
         }
 
         protected override void OnStart()
