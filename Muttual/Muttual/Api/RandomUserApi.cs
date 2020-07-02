b4a3629559f7ca52ce8a5a084f07eb2c33c50fd8
Rafael Valencia Castellanos <raf.valenc@gmail.com>
2020-07-01 23:58:25 -0500

    Se genera el consumo del api

---
+++ Muttual/Muttual/Api/RandomUserApi.cs
@@ -0,0 +1,38 @@
+﻿using Muttual.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using static Muttual.Model.PersonModel;
+
+namespace Muttual.Api
+{
+    public class RandomUserApi
+    {
+        public ObservableCollection<PersonModel> People { get; set; }
+        public RandomUserApi()
+        {
+            if (People == null)
+            {
+                People = new ObservableCollection<PersonModel>();
+                var list = GetManyDummyUser();
+                foreach (var item in list)
+                {
+                    People.Add(item);
+                }
+            }
+        }
+        public ObservableCollection<PersonModel> Consultar()
+        {
+            return People;
+        }
+        public void Add(PersonModel model)
+        {
+            People.Add(model);
+        }      
+    }
+}
