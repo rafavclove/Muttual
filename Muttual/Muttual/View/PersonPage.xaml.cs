73899f35dc5881d3651016e3233928acd26a7408
Rafael Valencia Castellanos <raf.valenc@gmail.com>
2020-07-01 23:58:36 -0500

    Se genera la vista

---
+++ Muttual/Muttual/View/PersonPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Muttual.Model;
+using Muttual.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Muttual.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonPage : ContentPage
+    {
+        PersonViewModel Context = new PersonViewModel();
+        public PersonPage()
+        {
+            InitializeComponent();
+            BindingContext = Context;
+        }
+    }
+}
